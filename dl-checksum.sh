50ec66ada189467a976ab9c1f035d7dbe2b9b5c0
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-02-01 16:55:47 -0500

    up to v3.11.5

---
+++ dl-checksum.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+MIRROR=http://www.apache.org/dist/cassandra
+
+dl_ver() {
+    local ver=$1
+    local url=$MIRROR/$ver/apache-cassandra-$ver-bin.tar.gz.sha512
+    printf "  # %s\n" $url
+    printf "  '%s': sha512:%s\n" $ver $(curl -sSL $url | awk '{print $1}')
+}
+
+dl_ver ${1:-3.11.5}
