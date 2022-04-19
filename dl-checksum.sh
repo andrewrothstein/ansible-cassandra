e0e8f61ecd75aa7459b9e4b77e31ffab9f9eb13b
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-04-19 01:03:08 -0400

    latest CI, deps, meta, cassandra:4.0.3

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 set -e
-MIRROR=http://www.apache.org/dist/cassandra
-
+MIRROR=http://downloads.apache.org/cassandra
+# https://downloads.apache.org/cassandra/4.0.3/apache-cassandra-4.0.3-bin.tar.gz.sha512
 dl_ver() {
     local ver=$1
-    local url=$MIRROR/$ver/apache-cassandra-$ver-bin.tar.gz.sha512
+    local shatype=${2:-sha512}
+    local url=$MIRROR/$ver/apache-cassandra-$ver-bin.tar.gz.${shatype}
     printf "  # %s\n" $url
-    printf "  '%s': sha512:%s\n" $ver $(curl -sSL $url | awk '{print $1}')
+    printf "  '%s': %s:%s\n" $ver $shatype $(curl -sSLf $url | awk '{print $1}')
 }
 
-dl_ver ${1:-4.0.1}
+dl_ver ${1:-4.0.3}
